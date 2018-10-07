5e58b184a2bcb609cbded4ad3cde7561cc04f8df
zahra <zahratee@gmail.com>
2018-10-07 19:49:51 +0100

    Wire up colour editng sliders

--- src/App.js
+++ src/App.js
@@ -3,14 +3,19 @@ import './App.css';
 import ColourInput from './ColourInput';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      pass: true
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <h1>a11ydentifier</h1>
-        <h2>The quick brown fox jumps</h2>
-        <h2>Pass: Grade AA</h2>
-        <ColourInput target='Background'/>
-        <ColourInput target='Text'/>
+        <h2>{this.state.pass ? 'Pass' : 'Fail'}</h2><h3>Grade AA</h3>
+        <ColourInput target='background'/>
+        <ColourInput target='text'/>
       </div>
     );
   }
--- src/ColourInput.js
+++ src/ColourInput.js
@@ -1,18 +1,58 @@
 import React, { Component } from 'react';
 
 export default class ColourInput extends Component {
+
+    constructor() {
+        super();
+        this.changeHSL = this.changeHSL.bind(this)
+        this.state = {
+            hue: 0,
+            saturation: 0,
+            brightness: 0,
+            hex: '#ABCDEF'
+        }
+    }
+
     render() {
         return (
-        <div>
-            <h3>{this.props.target}</h3>
-            <label>Hue</label>
-            <input id='hue' name='hue' type='range'/>
-            <label>Saturation</label>
-            <input id='saturation' name='saturation' type='range'/>
-            <label>Brightness</label>
-            <input id='brightness' name='brightness' type='range'/>
+        <div className="color-input">
+            <h4>{this.props.target}</h4>
+            <h3>{this.state.hex}</h3>
+            <div class='control'>
+                <label class='property'>Hue</label>
+                <label class='value'>{this.state.hue}</label>
+                <input id='hue' name='hue' type='range' min="0" max="360" value={this.state.hue} onChange={this.changeHSL}/>
+            </div>
+            <div class='control'>
+                <div class='labels'>
+                    <label class='property'>Saturation</label>
+                    <label class='value'>{this.state.saturation}</label>
+                    <input id='saturation' name='saturation' type='range' min="0" max="100" value={this.state.saturation} onChange={this.changeHSL}/>
+                </div>
+            </div>
+            <div class='control'>
+                <label class='property'>Brightness</label>
+                <label class='value'>{this.state.brightness}</label>
+                <input id='brightness' name='brightness' type='range' min="0" max="100" value={this.state.brightness} onChange={this.changeHSL}/>
+            </div>
         </div>
         );
-      }
+    }
+
+    changeHSL(event) {
+        const {value, id} = event.target;
+        let formattedValue = value;
+        if (id !== 'hue') {
+            formattedValue = `${value}%`;
+        }
+        const newState = {};
+        newState[id] = value;
+        this.setState(newState);
+        document.documentElement.style.setProperty(`--${this.props.target}_${id}`, formattedValue);
+    }
+
+    calculateContrast(){
+
+    }
 
 }
