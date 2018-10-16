e5834771919e89d419d3f71641da94e0cf5c1353
zahra <zahratee@gmail.com>
2018-10-16 15:44:40 +0100

    Calculate and display actual hex values

--- src/ColourInput.js
+++ src/ColourInput.js
@@ -18,7 +18,6 @@ export default class ColourInput extends Component {
         stylesheet.setProperty(`--${props.target}_brightness`, `${props.initialBrightness}%`);
     }
 
-
     render() {
         return (
         <div className="color-input">
@@ -44,19 +43,51 @@ export default class ColourInput extends Component {
     }
 
     changeHSL(event) {
-        const {value, id} = event.target;
-        let formattedValue = value;
-        if (id !== 'hue') {
-            formattedValue = `${value}%`;
-        }
+        let {value, id} = event.target;
+        value *= 1;
+
+        const formattedValue = id !== 'hue' ?  `${value}%` : value;
         const newState = {};
         newState[id] = value;
+
+        const hue = id === 'hue' ? value : this.state.hue;
+        const saturation = id === 'saturation' ? value : this.state.saturation;
+        const brightness = id === 'brightness' ? value : this.state.brightness;
+
+        newState.hex = this.hslToHex(hue, saturation, brightness);
+
         this.setState(newState);
         document.documentElement.style.setProperty(`--${this.props.target}_${id}`, formattedValue);
     }
 
-    calculateContrast(){
-
-    }
+    hslToHex(h, s, l) {
+        h /= 360;
+        s /= 100;
+        l /= 100;
+        let r, g, b;
+        if (s === 0) {
+          r = g = b = l; // achromatic
+        }
+        else {
+          const hue2rgb = (p, q, t) => {
+            if (t < 0) t += 1;
+            if (t > 1) t -= 1;
+            if (t < 1 / 6) return p + (q - p) * 6 * t;
+            if (t < 1 / 2) return q;
+            if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
+            return p;
+          };
+          const q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+          const p = 2 * l - q;
+          r = hue2rgb(p, q, h + 1 / 3);
+          g = hue2rgb(p, q, h);
+          b = hue2rgb(p, q, h - 1 / 3);
+        }
+        const toHex = x => {
+          const hex = Math.round(x * 255).toString(16);
+          return hex.length === 1 ? '0' + hex : hex;
+        };
+        return `#${toHex(r)}${toHex(g)}${toHex(b)}`;
+      }
 
 }
