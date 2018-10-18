07bb116eb36a01209a810e34f728f589e7882ea1
zahra <zahratee@gmail.com>
2018-10-18 16:43:33 +0100

    Format code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import './App.css';
-import ColourInput from './ColourInput';
+import React, { Component } from "react";
+import "./App.css";
+import ColourInput from "./ColourInput";
 
 class App extends Component {
   constructor() {
@@ -10,8 +10,8 @@ class App extends Component {
       rgbText: [1, 1, 1],
       rgbBackground: [0, 0, 0],
       contrast: 21,
-      pangram: this.getRandomPangram(),
-    }
+      pangram: this.getRandomPangram()
+    };
 
     this.contrast = this.contrast.bind(this);
     this.luminance = this.luminance.bind(this);
@@ -21,64 +21,79 @@ class App extends Component {
   }
 
   render() {
-    const {contrast, pangram} = this.state;
-
+    const { contrast, pangram } = this.state;
     return (
       <div className="App">
-        <div className='results'>
-          <span className='result'>{contrast >= 4.5 ? 'Pass' : 'Fail'}</span>
-          <span className='ratio'>{`${contrast.toFixed(1)}:1`}</span>
-          <span className='grade'>{contrast >= 7 ? 'AAA' : contrast >= 4.5 ? 'AA' : ''}</span>
+        <div className="results">
+          <span className="result">{contrast >= 4.5 ? "Pass" : "Fail"}</span>
+          <span className="ratio">{`${contrast.toFixed(1)}:1`}</span>
+          <span className="grade">
+            {contrast >= 7 ? "AAA" : contrast >= 4.5 ? "AA" : ""}
+          </span>
         </div>
-        <div className='results'>{pangram}</div>
-        <ColourInput target='background' initialHue={0} initialSaturation={50} initialBrightness={0} onColorChange={this.onBackgroundColorChange}/>
-        <ColourInput target='text' initialHue={0} initialSaturation={50} initialBrightness={100} onColorChange={this.onTextColorChange}/>
+        <div className="results">{pangram}</div>
+        <ColourInput
+          target="background"
+          initialHue={0}
+          initialSaturation={50}
+          initialBrightness={0}
+          onColorChange={this.onBackgroundColorChange}
+        />
+        <ColourInput
+          target="text"
+          initialHue={0}
+          initialSaturation={50}
+          initialBrightness={100}
+          onColorChange={this.onTextColorChange}
+        />
+        <div className="footer">
+          Made with ♥ by <a href="https://www.zahratraboulsi.com">Zahra</a> and{" "}
+          <a href="https://syeefkarim.com">Syeef</a>
+        </div>
       </div>
     );
   }
 
-
   contrast(rgb1, rgb2) {
-    return (this.luminance(rgb1[0], rgb1[1], rgb1[2]) + 0.05)
-        / (this.luminance(rgb2[0], rgb2[1], rgb2[2]) + 0.05);
+    return (
+      (this.luminance(rgb1[0], rgb1[1], rgb1[2]) + 0.05) /
+      (this.luminance(rgb2[0], rgb2[1], rgb2[2]) + 0.05)
+    );
   }
 
   onColorChange(rgb1, rgb2) {
     const contrast = this.contrast(rgb1, rgb2);
-    this.setState({contrast: contrast});
+    this.setState({ contrast: contrast });
   }
 
   onBackgroundColorChange(r, g, b) {
-    this.setState({rgbBackground: [r, g, b]})
+    this.setState({ rgbBackground: [r, g, b] });
     this.onColorChange(this.state.rgbText, [r, g, b]);
   }
 
   onTextColorChange(r, g, b) {
-    this.setState({rgbText: [r, g, b]});
+    this.setState({ rgbText: [r, g, b] });
     this.onColorChange([r, g, b], this.state.rgbBackground);
   }
 
   luminance(r, g, b) {
     const a = [r, g, b].map(v => {
-        return v <= 0.03928
-            ? v / 12.92
-            : Math.pow( (v + 0.055) / 1.055, 2.4 );
+      return v <= 0.03928 ? v / 12.92 : Math.pow((v + 0.055) / 1.055, 2.4);
     });
     return a[0] * 0.2126 + a[1] * 0.7152 + a[2] * 0.0722;
   }
 
   getRandomPangram() {
     const pangrams = [
-      'How quickly daft jumping zebras vex.',
-      'Go, lazy fat vixen; be shrewd, jump quick.',
-      'Fix problems quickly with galvanized jets.',
-      'Pack my red box with five dozen quality jugs.'
+      "How quickly daft jumping zebras vex.",
+      "Go, lazy fat vixen; be shrewd, jump quick.",
+      "Fix problems quickly with galvanized jets.",
+      "Pack my red box with five dozen quality jugs."
     ];
 
     const pangramIndex = Math.round(Math.random() * 3);
     return pangrams[pangramIndex];
   }
-
 }
 
 export default App;
--- src/ColourInput.js
+++ src/ColourInput.js
@@ -1,107 +1,142 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export default class ColourInput extends Component {
+  constructor(props) {
+    super();
+    this.changeHSL = this.changeHSL.bind(this);
+    this.state = {
+      hue: props.initialHue,
+      saturation: props.initialSaturation,
+      brightness: props.initialBrightness,
+      hex: this.hslToHex(
+        props.initialHue,
+        props.initialSaturation,
+        props.initialBrightness
+      )
+    };
 
-    constructor(props) {
-        super();
-        this.changeHSL = this.changeHSL.bind(this)
-        this.state = {
-            hue: props.initialHue,
-            saturation: props.initialSaturation,
-            brightness: props.initialBrightness,
-            hex: this.hslToHex(props.initialHue, props.initialSaturation, props.initialBrightness)
-        }
+    const stylesheet = document.documentElement.style;
+    stylesheet.setProperty(`--${props.target}_hue`, props.initialHue);
+    stylesheet.setProperty(
+      `--${props.target}_saturation`,
+      `${props.initialSaturation}%`
+    );
+    stylesheet.setProperty(
+      `--${props.target}_brightness`,
+      `${props.initialBrightness}%`
+    );
+  }
 
-        const stylesheet = document.documentElement.style;
-        stylesheet.setProperty(`--${props.target}_hue`, props.initialHue);
-        stylesheet.setProperty(`--${props.target}_saturation`, `${props.initialSaturation}%`);
-        stylesheet.setProperty(`--${props.target}_brightness`, `${props.initialBrightness}%`);
-    }
-
-    render() {
-        return (
-        <div className="color-input">
-            <h4>{this.props.target}</h4>
-            <h3>{this.state.hex}</h3>
-            <div className='control'>
-                <label className='property'>Hue</label>
-                <label className='value'>{this.state.hue}</label>
-                <input id='hue' name='hue' type='range' min="0" max="360" value={this.state.hue} onChange={this.changeHSL}/>
-            </div>
-            <div className='control'>
-                <label className='property'>Saturation</label>
-                <label className='value'>{this.state.saturation}</label>
-                <input id='saturation' name='saturation' type='range' min="0" max="100" value={this.state.saturation} onChange={this.changeHSL}/>
-            </div>
-            <div className='control'>
-                <label className='property'>Brightness</label>
-                <label className='value'>{this.state.brightness}</label>
-                <input id='brightness' name='brightness' type='range' min="0" max="100" value={this.state.brightness} onChange={this.changeHSL}/>
-            </div>
+  render() {
+    return (
+      <div className="color-input">
+        <h4>{this.props.target}</h4>
+        <h3 onClick={() => navigator.clipboard.writeText(this.state.hex)}>
+          {this.state.hex}
+        </h3>
+        <div className="control">
+          <label className="property">Hue</label>
+          <label className="value">{this.state.hue}</label>
+          <input
+            id="hue"
+            name="hue"
+            type="range"
+            min="0"
+            max="360"
+            value={this.state.hue}
+            onChange={this.changeHSL}
+          />
         </div>
-        );
+        <div className="control">
+          <label className="property">Saturation</label>
+          <label className="value">{this.state.saturation}</label>
+          <input
+            id="saturation"
+            name="saturation"
+            type="range"
+            min="0"
+            max="100"
+            value={this.state.saturation}
+            onChange={this.changeHSL}
+          />
+        </div>
+        <div className="control">
+          <label className="property">Brightness</label>
+          <label className="value">{this.state.brightness}</label>
+          <input
+            id="brightness"
+            name="brightness"
+            type="range"
+            min="0"
+            max="100"
+            value={this.state.brightness}
+            onChange={this.changeHSL}
+          />
+        </div>
+      </div>
+    );
+  }
+
+  changeHSL(event) {
+    let { value, id } = event.target;
+    value *= 1;
+
+    const formattedValue = id !== "hue" ? `${value}%` : value;
+    const newState = {};
+    newState[id] = value;
+
+    const hue = id === "hue" ? value : this.state.hue;
+    const saturation = id === "saturation" ? value : this.state.saturation;
+    const brightness = id === "brightness" ? value : this.state.brightness;
+
+    const [r, g, b] = this.hslToRgb(hue, saturation, brightness);
+
+    const hex = this.rgbToHex(r, g, b);
+    this.props.onColorChange(r, g, b);
+    newState.hex = hex;
+    this.setState(newState);
+    document.documentElement.style.setProperty(
+      `--${this.props.target}_${id}`,
+      formattedValue
+    );
+  }
+
+  hslToRgb(h, s, l) {
+    h /= 360;
+    s /= 100;
+    l /= 100;
+    let r, g, b;
+    if (s === 0) {
+      r = g = b = l; // achromatic
+    } else {
+      const hue2rgb = (p, q, t) => {
+        if (t < 0) t += 1;
+        if (t > 1) t -= 1;
+        if (t < 1 / 6) return p + (q - p) * 6 * t;
+        if (t < 1 / 2) return q;
+        if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
+        return p;
+      };
+      const q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+      const p = 2 * l - q;
+      r = hue2rgb(p, q, h + 1 / 3);
+      g = hue2rgb(p, q, h);
+      b = hue2rgb(p, q, h - 1 / 3);
     }
+    return [r, g, b];
+  }
 
-    changeHSL(event) {
-        let {value, id} = event.target;
-        value *= 1;
+  rgbToHex(r, g, b) {
+    const toHex = x => {
+      const hex = Math.round(x * 255).toString(16);
+      return hex.length === 1 ? "0" + hex : hex;
+    };
+    return `#${toHex(r)}${toHex(g)}${toHex(b)}`;
+  }
 
-        const formattedValue = id !== 'hue' ? `${value}%` : value;
-        const newState = {};
-        newState[id] = value;
-
-        const hue = id === 'hue' ? value : this.state.hue;
-        const saturation = id === 'saturation' ? value : this.state.saturation;
-        const brightness = id === 'brightness' ? value : this.state.brightness;
-
-        const [r, g, b] = this.hslToRgb(hue, saturation, brightness);
-
-        const hex = this.rgbToHex(r, g, b);
-        this.props.onColorChange(r, g, b);
-        newState.hex = hex;
-        this.setState(newState);
-        document.documentElement.style.setProperty(`--${this.props.target}_${id}`, formattedValue);
-    }
-
-    hslToRgb(h, s, l) {
-        h /= 360;
-        s /= 100;
-        l /= 100;
-        let r, g, b;
-        if (s === 0) {
-          r = g = b = l; // achromatic
-        }
-        else {
-          const hue2rgb = (p, q, t) => {
-            if (t < 0) t += 1;
-            if (t > 1) t -= 1;
-            if (t < 1 / 6) return p + (q - p) * 6 * t;
-            if (t < 1 / 2) return q;
-            if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
-            return p;
-          };
-          const q = l < 0.5 ? l * (1 + s) : l + s - l * s;
-          const p = 2 * l - q;
-          r = hue2rgb(p, q, h + 1 / 3);
-          g = hue2rgb(p, q, h);
-          b = hue2rgb(p, q, h - 1 / 3);
-        }
-        return [r, g, b];
-        
-    }
-
-    rgbToHex(r, g, b) {
-        const toHex = x => {
-            const hex = Math.round(x * 255).toString(16);
-            return hex.length === 1 ? '0' + hex : hex;
-          };
-        return `#${toHex(r)}${toHex(g)}${toHex(b)}`;
-    }
-
-    hslToHex(h, s, l) {
-        const rgb = this.hslToRgb(h, s, l);
-        const [r, g, b] = rgb;
-        return this.rgbToHex(r, g, b)
-    }
-
+  hslToHex(h, s, l) {
+    const rgb = this.hslToRgb(h, s, l);
+    const [r, g, b] = rgb;
+    return this.rgbToHex(r, g, b);
+  }
 }
